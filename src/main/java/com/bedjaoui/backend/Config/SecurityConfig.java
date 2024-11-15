cc90292cc5ad592546201b88cbb83ea6fa96b423
KhalilBDJ <khalilobedjaoui@gmail.com>
2024-11-15 14:14:44 +0100

    Security added but not working

--- src/main/java/com/bedjaoui/backend/Config/SecurityConfig.java
+++ src/main/java/com/bedjaoui/backend/Config/SecurityConfig.java
@@ -2,11 +2,53 @@ package com.bedjaoui.backend.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Désactiver la protection CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/login", "/users/register").permitAll() // Autoriser l'inscription et la connexion sans authentification
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // Restriction sur les endpoints d'admin
+                        .anyRequest().authenticated() // Toutes les autres requêtes nécessitent une authentification
+                )
+                .httpBasic(withDefaults()) // Utilisation de l'authentification HTTP basique pour l'administrateur (ou via un JWT si nécessaire)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Gestion des sessions stateless pour les API REST
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin_password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
